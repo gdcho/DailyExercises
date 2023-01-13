c3a63698bfc35a185abb90b71d56eeee4e189369
gdcho <48280799+gdcho@users.noreply.github.com>
2023-01-12 20:22:51 -0800

    Add files via upload

--- capital_indexes.py
+++ capital_indexes.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #2023-Jan-11
 #rjs
 #Python Programming Challenges (pythonprinciples.com/challenges/)
@@ -13,3 +14,20 @@ def capital_indexes(str):
             lst.append(x)
     return lst
 
+=======
+#2023-Jan-11
+#rjs
+#Python Programming Challenges (pythonprinciples.com/challenges/)
+
+#Capital Indexes
+#Write a function named capital_indexes. The function takes a single parameter, which is a string. 
+#Your function should return a list of all the indexes in the string that have capital letters.
+
+def capital_indexes(str):
+    lst = []
+    for x in range(len(str)):
+        if str[x].isupper() == True:
+            lst.append(x)
+    return lst
+
+>>>>>>> 148038b (Capital Indexes)
