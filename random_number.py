09af8d60e30bfd42a0b9474326f895b9a68b9f28
gdcho <48280799+gdcho@users.noreply.github.com>
2023-01-14 22:09:06 -0800

    Create random_number.py

---
+++ random_number.py
@@ -0,0 +1,14 @@
+#2023-Jan-14
+#rjs
+#Python Programming Challenges (pythonprinciples.com/challenges/)
+
+#Random Number
+#Define a function, random_number, that takes no parameters. The function must generate a random integer between 1 and 100, both inclusive, and return it.
+#Calling the function multiple times should (usually) return different numbers.
+
+
+import random
+def random_number():
+    return random.randint(1,100)
+    
+    
