d7f7bf679f8843a8ae502ef2995b2f8db0a7e267
gdcho <48280799+gdcho@users.noreply.github.com>
2023-01-15 22:11:24 -0800

    Create type_check.py

---
+++ type_check.py
@@ -0,0 +1,11 @@
+#2023-Jan-15
+#rjs
+#Python Programming Challenges (pythonprinciples.com/challenges/)
+
+#Type check
+#Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
+#For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+
+
+def only_ints(par1, par2):
+    return type(par1) == int and type(par2) == int
