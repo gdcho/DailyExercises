3b4465e6260566892b142afd4642e9e74c9709aa
David Cho <48280799+gdcho@users.noreply.github.com>
2023-01-25 21:29:19 -0800

    Create divisible_by_three.py

---
+++ divisible_by_three.py
@@ -0,0 +1,11 @@
+#2023-Jan-25
+#rjs
+#Python Programming Challenges (pythonprinciples.com/challenges/)
+
+#divisible_by_3
+
+#Define a function named div_3 that returns True if its single integer parameter is divisible by 3 and False otherwise.
+
+#My Solution
+def div_3(par):
+    return par % 3 == 0
