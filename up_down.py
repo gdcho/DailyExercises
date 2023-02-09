a38ac6bc1a08d4ca65f752aa9ac870dc129c9749
David Cho <48280799+gdcho@users.noreply.github.com>
2023-02-08 17:00:31 -0800

    Create up_down.py

---
+++ up_down.py
@@ -0,0 +1,7 @@
+##Up and down
+
+##Define a function named up_down that takes a single number as its parameter. 
+##Your function return a tuple containing two numbers; the first should be one lower than the parameter, and the second should be one higher.
+
+def up_down(num):
+    return (num-1, num+1)
